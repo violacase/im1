9dc117291cdc0c6f358637cc8612749d44a06235
violacase <violacase@gmail.com>
2025-08-04 12:40:21 +0200

    unplugins geinstalleerd

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { fileURLToPath, URL } from 'node:url'
-
+import AutoImport from 'unplugin-auto-import/vite'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
@@ -7,7 +7,16 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools(), tailwindcss()],
+  plugins: [
+    vue(),
+    AutoImport({
+      imports: ['vue', 'vue-router'],
+      dirs: ['src/composables', 'src/utils'],
+      dts: true, // Genereert TypeScript definities
+    }),
+    vueDevTools(),
+    tailwindcss(),
+  ],
   server: {
     port: 3000,
   },
